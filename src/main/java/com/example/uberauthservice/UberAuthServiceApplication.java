5b6a064c23702c65707579b6d72c18cf34015fda
Vivek Singh Pundir <132738914+vivekdevp01@users.noreply.github.com>
2025-08-04 14:43:03 +0530

    Added first commit

---
+++ src/main/java/com/example/uberauthservice/UberAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.uberauthservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UberAuthServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UberAuthServiceApplication.class, args);
+    }
+
+}
